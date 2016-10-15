84d5e529b5ae99bcbf8e828418103a69bbb32d42
faiaz <fayaz.sanaulla@gmail.com>
2016-10-15 14:53:21 +0300

    moving to class

--- src/main/scala/SparkParsing.scala
+++ src/main/scala/SparkParsing.scala
@@ -3,20 +3,24 @@ import org.apache.spark.{SparkConf, SparkContext}
 /**
   * Created by faiaz on 14.10.16.
   */
-object SparkParsing extends App {
+class SparkParsing {
 
-  val conf = new SparkConf()
+  private val conf = new SparkConf()
     .setMaster("local[4]")
     .setAppName("bigData")
     .set("spark.executor.memory", "1g")
 
-  val sc = new SparkContext(conf)
+  private val sc = new SparkContext(conf)
 
-  val file = sc.parallelize(Seq("Fayaz,", "Riaz,", "Anna"))
+  private var classpath = ""
 
-  val count = file.flatMap(line => line.split(","))
-    .map(word => (word, 1))
-    .reduceByKey(_ + _)
-  count.foreach(println)
+  def setFile(classPath: String) = classpath = classPath
+
+  def count = {
+    sc.textFile(classpath)
+      .flatMap(line => line.split(","))
+      .map(word => (word, 1))
+      .reduceByKey(_ + _)
+  }
 }
 
