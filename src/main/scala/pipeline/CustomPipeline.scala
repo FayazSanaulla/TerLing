2db93eeea6c05292da10e35153eb1f517bf2ee67
faiaz <fayaz.sanaulla@gmail.com>
2017-01-01 22:38:49 +0200

    textCleaner transformer impl

--- src/main/scala/pipeline/CustomPipeline.scala
+++ src/main/scala/pipeline/CustomPipeline.scala
@@ -3,14 +3,15 @@ package pipeline
 import config.SparkConfig
 import epic.preprocess.MLSentenceSegmenter
 import org.apache.spark.ml.feature.StopWordsRemover
+import pipeline.transformers.TextCleaner
 
 /**
   * Created by faiaz on 31.12.16.
   */
-object CustomePipeline extends App with SparkConfig {
+object CustomPipeline extends App with SparkConfig {
   import sqlContext.implicits._
 
-  private def removeUseless(str: String): String = {
+  private def removeUselessSymbols(str: String): String = {
     str.replaceAll("[,!?:]", "")
       .replaceAll("""\[[0-9]+]""", "")
       .replace("-", " ")
@@ -19,7 +20,7 @@ object CustomePipeline extends App with SparkConfig {
   val segmenter = MLSentenceSegmenter.bundled().get
 
   val text = sc.textFile("/home/faiaz/testData/scala.txt")
-    .map(removeUseless)
+    .map(removeUselessSymbols)
     .flatMap(segmenter)
     .map(_.replaceAll("\\.", ""))
     .map(_.split(" "))
@@ -29,6 +30,14 @@ object CustomePipeline extends App with SparkConfig {
     .setInputCol("sentences")
     .setOutputCol("filtered")
 
-  stopWordsRemover.transform(text).drop("sentences").show()
+  val test = sc.textFile("/home/faiaz/testData/scala.txt").flatMap(segmenter).toDF("sentences")
+
+  val textCleaner = new TextCleaner()
+    .setInputCol("sentences")
+    .setOutputCol("cleaned")
+
+  //stopWordsRemover.transform(text).drop("sentences").show()
+  //textCleaner.transform(test).show()
+  test.show()
 
 }
